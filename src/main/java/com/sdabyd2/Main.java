aaa9df3aa60dcc5352abf5946b002fe472f81f4d
Adrian Giełzak <adrian.gielzak@outlook.com>
2018-01-28 15:21:55 +0100

    Student

--- src/main/java/com/sdabyd2/Main.java
+++ src/main/java/com/sdabyd2/Main.java
@@ -1,7 +1,23 @@
 package com.sdabyd2;
 
+import com.sdabyd2.controller.StudentController;
+import com.sdabyd2.model.Student;
+import com.sdabyd2.view.StudentView;
+
 public class Main {
     public static void main(String[] args) {
+        Student model = retriveStudentFromDatabase();
+        StudentView view = new StudentView();
+        StudentController controller = new StudentController(model, view);
 
+        controller.updateView();
+
+    }
+
+    private static Student retriveStudentFromDatabase() {
+        Student student = new Student();
+        student.setName("Adrian");
+        student.setRollNo("5");
+        return student;
     }
 }
---
+++ src/main/java/com/sdabyd2/view/StudentView.java
@@ -0,0 +1,10 @@
+package com.sdabyd2.view;
+
+public class StudentView {
+
+    public void printStudentDetails(String studenName, String studentRollNo) {
+        System.out.println("Student: ");
+        System.out.println("Name: " + studenName);
+        System.out.println("Roll no. " + studentRollNo);
+    }
+}
